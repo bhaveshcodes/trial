eb1ef8df639246184fbe58db9a52bbc120013f87
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-18 00:15:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const app = express();
 
 const port = 9000;
 
-app.use("/", (req, res) => {
+app.use("/ok", (req, res) => {
   res.json({ message: "Hello Vercel" });
+  
 });
 
 app.listen(9000, () => {
   console.log(`Server is running on Port ${port}`);
 });
+
+
+app.use('/', (req, res) => {
+   
+    // let blogs = []
+    //  db.collection('blogs')
+    //     .find()
+    //     .sort({ _id: -1 })
+    //     .forEach(blog => blogs.push(blog))
+    //     .then(() => {
+             
+    //         res.json(blogs)
+             
+          
+    //     })
+    //     .catch((err) => {
+    //          res.status(500).json({ error: 'could not get data' })
+    //     })
+
+    res.json("hi!")
+})
