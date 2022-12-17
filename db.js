e397093e1fedf350440a01171e85bb80881aa67d
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-18 04:31:52 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const env =require('dotenv').config()
 // env
  
 let dbConnection
-let uri="mongodb+srv://mongoDB1:uE85CDXAgVnjzu1d@cluster0.i0bhhlu.mongodb.net/?retryWrites=true&w=majority"
+let uri="mongodb+srv://mongouser_bhavesh:xqSXyvoc2Fu5dZOw@courses-project-db-clus.pcfvfmr.mongodb.net/?retryWrites=true&w=majority"
 
  
 
